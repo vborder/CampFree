1394d4ed483b641de97c0c4ee1f8efd206454768
therandomfool <mddenver@protonmail.com>
2020-06-23 13:12:22 -0700

    Added Crossorigin annoatation

--- CampFree/src/main/java/com/skilldistillery/campfree/controllers/TestController.java
+++ CampFree/src/main/java/com/skilldistillery/campfree/controllers/TestController.java
@@ -1,11 +1,13 @@
 package com.skilldistillery.campfree.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin({"*", "http://localhost:4210"})
 public class TestController {
 	
 	@GetMapping("ping")
