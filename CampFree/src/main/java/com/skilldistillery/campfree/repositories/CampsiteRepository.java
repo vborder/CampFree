56eecd8dd3f9bf41463c7f92ebf754f81650db72
mikematosky <mikematosky@hotmail.com>
2020-06-24 17:53:21 -0400

    two tiny lines

--- CampFree/src/main/java/com/skilldistillery/campfree/repositories/CampsiteRepository.java
+++ CampFree/src/main/java/com/skilldistillery/campfree/repositories/CampsiteRepository.java
@@ -1,5 +1,9 @@
 package com.skilldistillery.campfree.repositories;
 
-public interface CampsiteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.campfree.entities.Campsite;
+
+public interface CampsiteRepository extends JpaRepository<Campsite, Integer>{
 
 }
