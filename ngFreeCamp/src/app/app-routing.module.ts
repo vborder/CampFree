7da24d173446c10144d4b2623c0d7d4fd0645629
therandomfool <mddenver@protonmail.com>
2020-06-25 11:14:49 -0700

    angular set up and google map api set up and display on campsite html

--- ngFreeCamp/src/app/app-routing.module.ts
+++ ngFreeCamp/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CampsiteComponent } from './components/campsite/campsite.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: CampsiteComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ngFreeCamp/src/app/app.module.ts
+++ ngFreeCamp/src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CampsiteComponent } from './components/campsite/campsite.component';
+import { CommentComponent } from './components/comment/comment.component';
+import { FeatureComponent } from './components/feature/feature.component';
+import { PersonComponent } from './components/person/person.component';
+import { PictureComponent } from './components/picture/picture.component';
+import { StateComponent } from './components/state/state.component';
+import { UserComponent } from './components/user/user.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CampsiteComponent,
+    CommentComponent,
+    FeatureComponent,
+    PersonComponent,
+    PictureComponent,
+    StateComponent,
+    UserComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    NgbModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBsCj7AmihtNZUsUjwnDrgZS57oy_QSJN4'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
