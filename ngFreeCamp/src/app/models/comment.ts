d02fac9b9788fd15f5e53a88609e58c1bead7711
therandomfool <mddenver@protonmail.com>
2020-06-26 13:29:12 -0700

    comment ts model complete

--- ngFreeCamp/src/app/models/comment.ts
+++ ngFreeCamp/src/app/models/comment.ts
@@ -1,2 +1,32 @@
+import { Campsite } from './campsite';
+import { Person } from './person';
+import { CampsiteService } from '../services/campsite.service';
+
 export class Comment {
+  id: number;
+  createdDateTime: number;
+  remark: string;
+  campsite: Campsite;
+  person: Person;
+  enabled: boolean;
+  campsiteRating: number;
+
+
+  constructor(
+    id?: number,
+    createdDateTime?: number,
+    remark?: string,
+    campsite?: Campsite,
+    person?: Person,
+    enabled?: boolean,
+    campsiteRating?: number
+  ){
+    this.id= id;
+    this.createdDateTime = createdDateTime;
+    this.remark = remark;
+    this.campsite = campsite;
+    this.person = person;
+    this.enabled = enabled;
+    this.campsiteRating = campsiteRating;
+  };
 }
