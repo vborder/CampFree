d94e9c7ea3973f977cf881f1385022afd7576330
therandomfool <mddenver@protonmail.com>
2020-06-26 13:46:02 -0700

    Feaature and person models complete

--- ngFreeCamp/src/app/models/person.ts
+++ ngFreeCamp/src/app/models/person.ts
@@ -1,2 +1,37 @@
+import { User } from './user';
+import { State } from './state';
+import { EmailValidator } from '@angular/forms';
+import { stringify } from 'querystring';
+
 export class Person {
+  id: number;
+  email: string;
+  firstName: string;
+  lastName: string;
+  profileImage: string;
+  user: User;
+  state: State;
+  bio: string;
+
+
+  constructor(
+    id?: number,
+    email?: string,
+    firstName?: string,
+    lastName?: string,
+    profileImage?: string,
+    user?: User,
+    state?: State,
+    bio?: string
+  ){
+    this.id = id;
+    this.email = email;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.profileImage =  profileImage;
+    this.user = user;
+    this.state = state;
+    this.bio = bio;
+
+  }
 }
