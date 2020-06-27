3897bcfeb59de4de60ecec2ee2431f73d79a155f
therandomfool <mddenver@protonmail.com>
2020-06-27 15:35:04 -0700

    Comment tables and buttons on campsite page

--- ngFreeCamp/src/app/models/campsite.ts
+++ ngFreeCamp/src/app/models/campsite.ts
@@ -9,19 +9,21 @@ export class Campsite {
   state: State;
   features: string;
   pictureUrl: string;
+  remark: string;
 
 
 
   constructor(id?: number, name?: string, location?: string, latitude?: number,
-              longitude?: number, state?: State, features?: string, pictureUrl?: string){
+              longitude?: number, state?: State, features?: string, pictureUrl?: string, remark?: string){
       this.id = id;
-		    this.name = name;
-		    this.location = location;
-		    this.latitude = latitude;
-		    this.longitude = longitude;
-		    this.state = state;
-		    this.features = features;
-		    this.pictureUrl = pictureUrl;
+		  this.name = name;
+		  this.location = location;
+		  this.latitude = latitude;
+		  this.longitude = longitude;
+		  this.state = state;
+		  this.features = features;
+      this.pictureUrl = pictureUrl;
+      this.remark = remark;
   }
 
 }
