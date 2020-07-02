7b90e05c77a7c1587264eddd7ce66d934d212c57
mikematosky <mikematosky@hotmail.com>
2020-07-02 11:46:48 -0400

    Gradle WAR task, prep for tomcat deploy

--- CampFree/src/main/java/com/skilldistillery/campfree/CampFreeApplication.java
+++ CampFree/src/main/java/com/skilldistillery/campfree/CampFreeApplication.java
@@ -2,13 +2,21 @@ package com.skilldistillery.campfree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class CampFreeApplication {
+public class CampFreeApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CampFreeApplication.class);
+    }
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CampFreeApplication.class, args);
 	}
